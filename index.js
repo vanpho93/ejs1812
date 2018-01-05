8bbc164a39a2637862b316c0ea4da7a121b24167
Van Pho Nguyen <vanpho02@gmail.com>
2018-01-05 20:48:56 +0700

    classes

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const reload = require('reload');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.render('home'));
+reload(app);
+
+app.get('/', (req, res) => {
+    const data = {
+        name: 'Pho',
+        age: 18,
+        isAdmin: false,
+        subjects: ['NodeJS', 'Javascript', 'Angular', 'Mongo']
+    };
+    res.render('home', data);
+});
+
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const users = [
+    new User('Teo', 10),
+    new User('Ti', 15),
+    new User('Tun', 12),
+];
+
+app.get('/user', (req, res) => {
+    res.render('list', { users });
+});
 
 app.listen(3000, () => console.log('Server started!'));
