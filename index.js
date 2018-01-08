0de5a78e559d16e3d3f73bd370c643fbf0412c0a
Van Pho Nguyen <vanpho02@gmail.com>
2018-01-08 20:35:29 +0700

    name

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const reload = require('reload');
+const parser = require('body-parser').urlencoded({ extended: false });
+
 const User = require('./models/User');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
 reload(app);
 
 const users = [
@@ -13,7 +14,6 @@ const users = [
     new User('Ti', 15),
     new User('Tun', 12)
 ];
-
 app.get('/', (req, res) => res.render('home'));
 
 app.get('/add/:name/:age', (req, res) => {
@@ -21,6 +21,14 @@ app.get('/add/:name/:age', (req, res) => {
     const user = new User(name, age);
     users.push(user);
     res.send('Them thanh cong');
+});
+
+app.post('/add', parser, (req, res) => {
+    const { name, age } = req.body;
+    const user = new User(name, age);
+    users.push(user);
+    // res.send('Them thanh cong');
+    res.redirect('/user');
 });
 
 app.get('/user', (req, res) => res.render('list', { users }));
